19d5cb937aa35983244a58a00a377da8885e158e
醒白 <469522336@qq.com>
2018-05-10 16:40:48 +0800

    修改

--- src/main/java/com/imooc/xunwuproject/service/house/AddressServiceImpl.java
+++ src/main/java/com/imooc/xunwuproject/service/house/AddressServiceImpl.java
@@ -29,7 +29,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.parser.Entity;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
